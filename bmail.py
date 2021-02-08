a94c4e62a5b4979eb2cb1c541831d9db549a4a62
Mehedi Hasan Ariyan <64999484+BotolMehedi@users.noreply.github.com>
2021-02-08 16:44:26 +0600

    Create bmail.py

---
+++ bmail.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os,sys,time,bain
+print
+print
+print('ALL PACKAGES ARE INSTALLING. PLEASE WAIT 1 OR 2 MINUTES.....')
+print
+print
+os.system('pkg install pip && pkg install pip2')
+time.sleep(1)
+
+os.system('pip install bain')
+time.sleep(1)
+
+os.system('pip2 install bain')
+time.sleep(1)
+
+os.system("bain")
+time.sleep(1)
