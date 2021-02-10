38def4ad9d2158bc5ca191dd4aefa4f55eb85403
Mehedi Hasan Ariyan <64999484+BotolMehedi@users.noreply.github.com>
2021-02-10 12:44:13 +0600

    Update bmail.py

--- bmail.py
+++ bmail.py
@@ -6,14 +6,15 @@ os.system('clear')
 print('ALL PACKAGES ARE INSTALLING. PLEASE WAIT 1 OR 2 MINUTES.....')
 print
 print
+os.system('clear')
 os.system('pkg install pip && pkg install pip2')
 time.sleep(1)
-
+os.system('clear')
 os.system('pip install bain')
 time.sleep(1)
-
+os.system('clear')
 os.system('pip2 install bain')
 time.sleep(1)
-
+os.system('clear')
 os.system("bain")
 time.sleep(1)
